455b17efbb12ae7ad4bba93ea84ddf2834961403
AbhishekPipo <Pipo.abhi@gmail.com>
2023-07-03 17:23:33 +0530

    3-7-23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-switch';
+  // title = 'ng-switch';
+  selectedOption: string = '';
+
+  setSelectedOption(option: string) {
+    this.selectedOption = option;
+  }
 }
